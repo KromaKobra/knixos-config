9497c38916db6b81b5c491d9062e6e0c6c639f63
Kroma Kobra <kromakobra@gmail.com>
2026-06-29 17:01:54 -0700

    init commit

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "KnixOS Configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, caelestia-shell, spicetify-nix, ... }@inputs:
+  {
+    nixosConfigurations.nixos =
+      nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = false;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.kroma = { ... }: {
+              imports = [
+                caelestia-shell.homeManagerModules.default
+                spicetify-nix.homeManagerModules.default
+                ./home.nix
+              ];
+            };
+          }
+        ];
+      };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,131 @@
+{ pkgs, inputs, ... }:
+
+{
+  home.username = "kroma";
+  home.homeDirectory = "/home/kroma";
+
+  home.stateVersion = "26.05";
+
+  # Dark mode fix on thunar I think
+  gtk = {
+    enable = true;
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+  # Dark mode fix END
+
+
+  ### PROGRAMS ###
+  programs.home-manager.enable = true;
+
+  programs.caelestia = {
+    enable = true;
+    cli.enable = true;
+  };
+
+  programs.fish.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "";
+      plugins = [
+        "git"
+        "sudo"
+        "zsh-autosuggestions"
+        "zsh-syntax-highlighting"
+      ];
+    };
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.zsh-autosuggestions;
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
+      }
+    ];
+    initExtra = ''
+      # --- Fancy launch screen ---
+      if [[ -o interactive ]] && [[ -z "$FASTFETCH_SHOWN" ]]; then
+        export FASTFETCH_SHOWN=1
+        fastfetch
+      fi
+
+      # --- Custom minimal prompt: path + git branch ---
+      autoload -Uz vcs_info
+      precmd_vcs_info() { vcs_info }
+      precmd_functions+=( precmd_vcs_info )
+
+      setopt PROMPT_SUBST
+      zstyle ':vcs_info:git:*' formats ' %F{3}(%b)%f'
+      zstyle ':vcs_info:*' enable git
+
+      PROMPT='%F{4}%~%f''${vcs_info_msg_0_} %F{2}〉%f'
+    '';
+  };
+
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblockify
+        hidePodcasts
+        shuffle
+      ];
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+    };
+
+
+  ### PACKAGES ###
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    quickshell
+
+    kitty       	# terminal
+    firefox     	# web browser
+    obsidian    	# notetaking
+    vscodium    	# code editor
+    pkgs.claude-code	# ai client
+    pkgs.thunar 	# file browser
+    libreoffice 	# doc editor suite
+    lunar-client 	# minecraft :)
+
+    fastfetch
+    app2unit
+
+    material-symbols
+    jetbrains-mono
+
+    wl-clipboard
+    grim
+    slurp
+    unzip
+
+    pavucontrol	# audio controls 
+    nomacs	# media viewer
+
+    # Cyber
+    bind	# nslookup, etc.
+  ];
+}
